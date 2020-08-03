00abaa12638c2e18e8836fba329a81afc3ab8904
shzeng <shzeng@thoughtworks.com>
2020-08-03 09:39:00 +0800

    [shzeng] feat: add jest for unit test.

---
+++ src/app/__tests__/app.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import App from '../app';
+
+describe('App', () => {
+  test('should render App component', () => {
+    render(<App />);
+    expect(screen.getByTestId('app')).toBeInTheDocument;
+  })
+});
--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div data-testid="app">Hello World</div>
     );
   }
 }
