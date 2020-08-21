75e1320128d47738d43e38a243bbea5bdf2cecde
shzeng <shzeng@thoughtworks.com>
2020-08-21 09:33:07 +0800

    [shzeng] fix: fix test.

--- src/app/__tests__/app.test.js
+++ src/app/__tests__/app.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from '../app';
 
 describe('App', () => {
   test('should render App component', () => {
-    render(<App />);
-    expect(screen.getByTestId('app')).toBeInTheDocument();
+    const { getByTestId } = render(<App />);
+    expect(getByTestId('app').textContent).toBe('Hello World');
   });
 });
