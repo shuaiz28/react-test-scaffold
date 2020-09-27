276619bd8adc95c37571c681efade436f4390bd1
yzqi <yzqi@thoughtworks.com>
2020-09-27 10:48:57 +0800

    [yanzi] #N/A test: add e2e tests for register page.

---
+++ cypress/integration/register_spec.js
@@ -0,0 +1,42 @@
+describe('Register page', () => {
+  it('should turn to login page', () => {
+    cy.visit('/register');
+    cy.contains('Cancel').click();
+    cy.url().should('include', '/login');
+  });
+
+  it('should show error message when required message is empty', () => {
+    cy.visit('/register');
+
+    cy.get('button:contains("Register")').click();
+
+    cy.contains('First Name is required');
+    cy.contains('Last Name is required');
+    cy.contains('Username is required');
+    cy.contains('Password is required');
+  });
+
+  it('should show error message when partial required message is empty', () => {
+    cy.visit('/register');
+    cy.get('input[name=firstName]').type('name');
+
+    cy.get('button:contains("Register")').click();
+
+    cy.contains('Last Name is required');
+    cy.contains('Username is required');
+    cy.contains('Password is required');
+  });
+
+  it('should show register successfully', () => {
+    cy.visit('/register');
+    cy.get('input[name=firstName]').type('firstName');
+    cy.get('input[name=lastName]').type('lastName');
+    cy.get('input[name=username]').type('username');
+    cy.get('input[name=password]').type('password');
+
+    cy.get('button:contains("Register")').click();
+
+    cy.url().should('include', '/login');
+    cy.contains('Registration successful')
+  });
+});
