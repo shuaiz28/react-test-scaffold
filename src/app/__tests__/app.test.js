eb30c1687da4e62133d9b9fc1760fb7efcc72561
yzqi <yzqi@thoughtworks.com>
2020-09-29 00:01:41 +0800

    [yanzi] #N/A test: add test-library test for login failed.

--- src/app/__tests__/app.test.js
+++ src/app/__tests__/app.test.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux'
+import configureStore from 'redux-mock-store'
+import 'babel-polyfill'
 
-import App from '../app';
+import { LoginPage } from '../LoginPage';
+import { BrowserRouter } from 'react-router-dom';
 
-describe('App', () => {
-  test('should render App component', () => {
-    const { getByTestId } = render(<App />);
-    expect(getByTestId('app').textContent).toBe('Hello World');
+describe('LoginPage', () => {
+  const initialState = { authentication: { } };
+  const mockStore = configureStore();
+  let store;
+
+  test('should show validation message', async () => {
+    store = mockStore(initialState)
+    const { getByTestId, findByText } = render(<Provider store={store}><BrowserRouter><LoginPage /></BrowserRouter></Provider>);
+
+    fireEvent.click(getByTestId('login'), 'submit');
+
+    await findByText(/Username is required/);
+    await findByText(/Password is required/);
   });
 });
