ddd1da36c9c8945f6afd1cc7f246416f771e9911
yzqi <yzqi@thoughtworks.com>
2020-09-29 11:58:23 +0800

    [yanzi] #N/A test: add test-library test for partial input when login.

--- src/app/__tests__/app.test.js
+++ src/app/__tests__/app.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { Provider } from 'react-redux'
 import configureStore from 'redux-mock-store'
 import 'babel-polyfill'
@@ -19,6 +19,18 @@ describe('LoginPage', () => {
     fireEvent.click(getByTestId('login'), 'submit');
 
     await findByText(/Username is required/);
+    await findByText(/Password is required/);
+  });
+
+  test('should show password validation message', async () => {
+    store = mockStore(initialState);
+    const { getByTestId, findByText, getByText, queryByText } = render(<Provider store={store}><BrowserRouter><LoginPage /></BrowserRouter></Provider>);
+    fireEvent.change(getByTestId('username'), { target: { value: 'username1' } });
+
+    fireEvent.click(getByTestId('login'), 'submit');
+
+    const result = screen.queryAllByText(/Username is required/);
+    expect(result).toEqual([]);
     await findByText(/Password is required/);
   });
 });
